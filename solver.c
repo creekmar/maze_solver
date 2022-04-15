b40c403f4301c7089d5be231aa57abf8473b70ba
Ming E Creekmore <mec5765@cs.rit.edu>
2022-04-15 12:37:42 -0400

    Removed function construct_path in solver. Make BFS construct path in another specific maze solver file

--- solver.c
+++ solver.c
@@ -10,24 +10,9 @@
 #include "solver.h"
 #include "QueueADT.h"
 
-QueueADT construct_path(QueueADT hashmap, void *start, void *end, void* (*prev) (void*)) {
-    QueueADT path = que_create(0, del, 0);
-    if(que_remove(hashmap, end) != 0) {
-        void * current = end;
-        while(current != start) {
-            que_insert(path, current);
-            current = prev(current); 
-            assert(current != 0);
-            que_remove(hashmap, current);
-        }
-        que_remove(hashmap, start);
-        que_insert(path, start);
-    }
-    que_destroy(hashmap);
-    return path;
-}
-
-QueueADT solve(void * start, void * config, void (*del) (void *a), QueueADT (*getNeigh) (void *b, void *c), bool (*isSolution) (const void *d, const void *e), bool (*equals) (const void *f, const void *g), void* (*prev) (void*)) {
+// solve finds a solution to a given configuration given helper functions and 
+// the start point
+void** solve(void * start, void * config, void (*del) (void *a), QueueADT (*getNeigh) (void *b, void *c), bool (*isSolution) (const void *d, const void *e), bool (*equals) (const void *f, const void *g)) {
     QueueADT queue = que_create(0, del, 0);
     que_insert(queue, start);
     QueueADT hashmap = que_create(0, del, equals);
@@ -54,12 +39,16 @@ QueueADT solve(void * start, void * config, void (*del) (void *a), QueueADT (*ge
         que_destroy(successor);
     }
     
+    //destroy allocated memory if no solution
     que_destroy(queue);
     if(!isSolution(config, current)) {
         que_destroy(hashmap);
         return 0;
     }
     else {
-        return construct_path(hashmap, start, current, prev);
+        void **pointers = (void**)malloc(sizeof(void*) * 2);
+        pointers[0] = (void*) hashmap;
+        pointers[1] = current;
+        return pointers;
     }
 }
--- solver.h
+++ solver.h
@@ -13,20 +13,6 @@
 
 #include "QueueADT.h"
 
-/// construct_path constructs the shortest path to the solution given
-/// a hashmap, start and end value, as well as the function that
-/// gets the previous value of a data in a queue
-/// Also deallocates the hashmap after removing the values that 
-/// pertain to the path
-///
-/// @param hashmap all of the possible pathways given from first part of BFS
-/// @param start the start data point of the path
-/// @param end the end data point of the path
-/// @param prev pointer to function that the data point previous to the
-///     current data point (example: backtracking your path)
-/// @return a queue of the path in correct order
-QueueADT construct_path(QueueADT hashmap, void *start, void *end, void* (*prev) (void*));
-
 /// solve solves a puzzle for the shortest solution using BFS
 ///
 /// @param start the start data point of the path
@@ -41,7 +27,6 @@ QueueADT construct_path(QueueADT hashmap, void *start, void *end, void* (*prev) 
 /// @param prev pointer to the function that gets the previous data point based
 ///     on the current data point (ex. backtracking your path)
 /// @return a queue of all the steps in the solution
-QueueADT solve(void *start, void *config, void (*del)(void *a), QueueADT (*getNeigh) (void *b, void *c), bool (*isSolution) (const void *d, const void *e), bool (*equals) (const void *f, const void *g), void* (*prev) (void*));
-
+QueueADT solve(void *start, void *config, void (*del)(void *a), QueueADT (*getNeigh) (void *b, void *c), bool (*isSolution) (const void *d, const void *e), bool (*equals) (const void *f, const void *g));
 
 
